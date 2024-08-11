12e57715736aa1eb253b889bd81842051df0b42f
Javier García <93519773+oviwanazul124@users.noreply.github.com>
2024-08-11 09:18:43 +0200

    Added Messages

--- NewCalculator.py
+++ NewCalculator.py
@@ -1,9 +1,14 @@
 
+# This function is in charge of sending to console the welcome message
+
 def welcome():
 
     print('\nWelcome to the calculator please enter the follwings symbols to make a calculation: \n "+" Use the plus symbol to do addition \n "-" Use the minus symbol to do subtraction \n "*" Use the  symbol to multiply \n "sqr" Use the string to do roots \n "**" Use the double  symbol to exponent')
     global symbolCalculation
     symbolCalculation = input("\nPlease enter the following: ")
+
+# The inputManager function work from getting a value, from example: value = n. Meaning that n is the maximun
+# number of inputs that the function can make.
 
 def inputManager(values):
     
@@ -28,6 +33,9 @@ def inputManager(values):
             inputThirdNumber = int(input('Please enter the third exponent to do the operation: '))
 
     pass
+
+# The calculation() is a function that recives a value from the variable 'symbolCalculation'. From this variable
+# check in match if the function is inside of the main symbol.
 
 def calculation():
 
@@ -73,6 +81,9 @@ def calculation():
 
             print("The input you has used " + str(symbolCalculation) + " is not defined in this calculator")
             welcome()
+
+# The function sqrt(), is a fuction that permit to use roots without the use of the library math.sqrt(), why
+# I implemented this?. Why not?.
 
 def sqrt(numerator, denominator, exponent):
     
